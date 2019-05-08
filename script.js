e3e9e0f298aa02430dd1d32c028ae6c1c5be7e8f
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-05-08 11:45:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let image = new Image();
+image.src = "cards.png";
+
+image.addEventListener('load',()=>{
+// begin hier jouw script
+})
